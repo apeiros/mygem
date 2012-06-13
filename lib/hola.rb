7afdf1948a05405202c6a11b3966b2c4b300d36a
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-06-13 13:48:07 +0200

    Moved the require to the top. Requires should be at the top of the file.

--- lib/hola.rb
+++ lib/hola.rb
@@ -1,12 +1,12 @@
 #!/home/user1/.rvm/rubies/ruby-1.9.3-p194/bin/ruby
 
+require_relative 'hola/translator'
+
 class Hola
   def self.hi(language = :english)
     translator = Translator.new(language)
     translator.hi
   end
 end
-
-require_relative 'hola/translator'
 
 puts Hola.hi(:english)
